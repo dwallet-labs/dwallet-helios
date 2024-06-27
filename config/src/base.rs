00573fec5758c846ccc84cbf8f1467c72dce5569
Yuval Elmoalem <yuval.elmo@dwalletlabs.com>
2024-06-27 15:26:48 +0300

    ethereum devnet configuration is taken from yml now - need to provide filename and place it on same folder of binary. also added example eth_config_devnet.yml

--- config/src/base.rs
+++ config/src/base.rs
@@ -1,13 +1,16 @@
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 use std::default::Default;
+use std::fs;
 use std::net::{IpAddr, Ipv4Addr};
 use std::path::PathBuf;
+use eyre::Report;
 
+use crate::utils::bytes_deserialize;
 use crate::types::{ChainConfig, Forks};
 use crate::utils::bytes_serialize;
 
 /// The base configuration for a network.
-#[derive(Serialize)]
+#[derive(Serialize, Deserialize)]
 pub struct BaseConfig {
     pub rpc_bind_ip: IpAddr,
     pub rpc_port: u16,
@@ -19,9 +22,13 @@ pub struct BaseConfig {
     pub default_checkpoint: Vec<u8>,
     pub chain: ChainConfig,
     pub forks: Forks,
+    #[serde(default)]
     pub max_checkpoint_age: u64,
+    #[serde(default)]
     pub data_dir: Option<PathBuf>,
+    #[serde(default)]
     pub load_external_fallback: bool,
+    #[serde(default)]
     pub strict_checkpoint_age: bool,
 }
 
@@ -39,5 +46,20 @@ impl Default for BaseConfig {
             load_external_fallback: false,
             strict_checkpoint_age: false,
         }
+    }
+}
+
+impl BaseConfig {
+    pub fn from_yaml_file(relative_path: &str) -> Result<Self, Report> {
+        let mut path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        path.push(relative_path);
+
+        let file_content = fs::read_to_string(path)?;
+        let mut config: BaseConfig = serde_yaml::from_str(&file_content)?;
+
+        if config.max_checkpoint_age == 0 {
+            config.max_checkpoint_age = 1_209_600; // 14 days
+        }
+        Ok(config)
     }
 }
