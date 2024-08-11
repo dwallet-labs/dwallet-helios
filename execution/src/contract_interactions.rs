ee822d58b18998fe6800925ee87fc355e0f2d6e3
Yuval Elmoalem <82144075+yuvalelmo@users.noreply.github.com>
2024-08-11 18:07:26 +0300

    dwallet-helios: add `contract_interactions` functions & tests (#4)
    Co-authored-by: Zeev Manilovich <zeevmanilovich@gmail.com>
    Co-authored-by: Zeev Manilovich <zeevmoney@users.noreply.github.com>
    Co-authored-by: Yehonatan Cohen Scaly <scalys7@gmail.com>

---
+++ execution/src/contract_interactions.rs
@@ -0,0 +1,156 @@
+//! This module provides utilities for standardizing and calculating storage slots in Solidity
+//! smart contracts. These utilities are essential for interacting with smart contract storage
+//! in a predictable and reliable manner. The utilities include functions for calculating mapping
+//! slots, and determining the storage slot for a given message and dWallet ID.
+//! These functions follow the specifications and layout described in Solidity's documentation
+//! for storage slots and mapping layouts.
+//!
+//! Solidity uses 256-bit hashes (H256) for storage slots and keys.
+//! When converting integers or other types of data to fit this format, padding is required
+//! to ensure they match the expected 256-bit length.
+//!
+//! The code in this module originates from the need to interact with smart contract storage
+//! layouts programmatically, ensuring consistency and correctness in storage access patterns.
+//! The implementation follows the specifications outlined in the
+//! [Solidity documentation](https://docs.soliditylang.org/en/v0.8.24/internals/layout_in_storage.html#mappings-and-dynamic-arrays)
+
+use ethers::{
+    abi::ethabi::{encode, Token},
+    types::H256,
+    utils::keccak256,
+};
+use eyre::Error;
+
+/// Calculates the mapping slot for a given key and storage slot (in the contract's storage layout).
+/// First initializes a new `Keccak256` hasher, then standardizes the input slot and key.
+/// The standardized key and slot are then hashed together to produce a new `H256` hash.
+/// The result hash will be used to get the location of the (key, value) pair in the contract's
+/// storage.
+/// # Arguments
+/// * `key` – A H256 hash that represents the key for which the mapping slot is to be calculated.
+///   The Key is `Keccak256(message + dwallet_id)`.
+/// * `Mapping_slot` – A `u64` value that represents the mapping slot in the contract storage layout.
+///   [For more info](https://docs.soliditylang.org/en/v0.8.24/internals/layout_in_storage.html#mappings-and-dynamic-arrays)
+#[allow(unused)]
+fn calculate_mapping_slot_for_key(key: H256, mapping_slot: u64) -> H256 {
+    let slot_encoded = encode(&[Token::Uint(mapping_slot.into())]);
+    let slot_encoded = H256::from_slice(&slot_encoded);
+
+    let key_encoded = encode(&[Token::FixedBytes(key.as_bytes().to_vec())]);
+    let key_encoded = H256::from_slice(&key_encoded);
+
+    let mut hasher = Vec::new();
+    hasher.extend_from_slice(key_encoded.as_bytes());
+    hasher.extend_from_slice(slot_encoded.as_bytes());
+    H256::from_slice(&keccak256(&hasher))
+}
+
+/// Calculates the key for a given message and dWallet ID.
+/// In the smart contract, the key is calculated by hashing the message, and the dWallet ID
+/// together. The result is a H256 hash that represents the key.
+#[allow(unused)]
+fn calculate_key(mut message: Vec<u8>, dwallet_id: Vec<u8>) -> H256 {
+    message.extend_from_slice(dwallet_id.as_slice());
+    H256::from_slice(&keccak256(&message))
+}
+
+/// Calculates the storage slot for a given message, dWallet ID, and data slot.
+/// The function first calculates a key by hashing the message, and the dWallet ID together.
+/// Then, it calculates the mapping slot for the calculated key, and the provided data slot.
+/// The calculated mapping slot can be used to locate the (key, value) pair in the contract's
+/// storage. # Arguments
+/// * `message` – A string that represents the message to be stored.
+/// * `dwallet_id` – A vector of bytes that represents the dWallet ID.
+/// * `data_slot` – An unsigned 64-bit integer that represents the data slot.
+/// # Returns
+/// * A `Result` that contains a 256-bit hash (H256) that represents the calculated storage slot, or
+///   an `Error` if the calculation fails.
+#[allow(unused)]
+pub fn get_message_storage_slot(
+    message: String,
+    dwallet_id: Vec<u8>,
+    data_slot: u64,
+) -> Result<H256, Error> {
+    // Calculate memory slot.
+    // Each key is calculated by hashing the concatenation of the msg and dWalletID.
+    let key = calculate_key(
+        message.clone().as_bytes().to_vec(),
+        dwallet_id.as_slice().to_vec(),
+    );
+    Ok(calculate_mapping_slot_for_key(key, data_slot))
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn calculate_mapping_slot_valid() {
+        let key = H256::from_slice(&[0u8; 32]);
+        let slot = 0;
+
+        let expected_hash = {
+            let mut hasher = Vec::new();
+            hasher.extend([0u8; 32]);
+            hasher.extend([0u8; 32]);
+            H256::from_slice(keccak256(&hasher).as_slice())
+        };
+
+        assert_eq!(expected_hash, calculate_mapping_slot_for_key(key, slot));
+
+        let key = H256::from_slice(&[1u8; 32]);
+        let slot = u64::MAX;
+
+        let expected_hash = {
+            let mut hasher = Vec::new();
+            hasher.extend([1u8; 32]);
+            hasher.extend([
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 255, 255,
+                255, 255, 255, 255, 255, 255,
+            ]);
+            H256::from_slice(keccak256(&hasher).as_slice())
+        };
+
+        assert_eq!(expected_hash, calculate_mapping_slot_for_key(key, slot));
+    }
+
+    #[test]
+    fn calculate_key_valid() {
+        let message: Vec<u8> = vec![];
+        let dwallet_id: Vec<u8> = vec![];
+        let expected_hash = { H256::from_slice(&keccak256(Vec::new())) };
+
+        assert_eq!(expected_hash, calculate_key(message, dwallet_id));
+
+        let dwallet_id = "be344ddffaa7a8c9c5ae7f2d09a77f20ed54f93bf5e567659feca5c3422ae7a6";
+        let byte_vec_dwallet_id = hex::decode(dwallet_id).expect("Invalid hex string");
+        let message = [1u8; 32].to_vec();
+
+        let expected_hash = {
+            let mut combined = message.clone();
+            combined.extend_from_slice(&byte_vec_dwallet_id);
+            H256::from_slice(keccak256(&combined).as_slice())
+        };
+
+        assert_eq!(expected_hash, calculate_key(message, byte_vec_dwallet_id))
+    }
+
+    #[test]
+    fn get_message_storage_slot_valid() {
+        let message = "test_message".to_string();
+        let dwallet_id = "be344ddffaa7a8c9c5ae7f2d09a77f20ed54f93bf5e567659feca5c3422ae7a6";
+        let byte_vec_dwallet_id = hex::decode(dwallet_id).expect("Invalid hex string");
+        let data_slot = 12345u64;
+
+        let key = calculate_key(
+            message.clone().as_bytes().to_vec(),
+            byte_vec_dwallet_id.clone(),
+        );
+        let expected_slot = calculate_mapping_slot_for_key(key, data_slot);
+
+        let result = get_message_storage_slot(message, byte_vec_dwallet_id, data_slot).unwrap();
+
+        assert_eq!(expected_slot, result);
+    }
+}
