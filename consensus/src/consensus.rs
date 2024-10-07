fa3ba3af673c5f789b007ac9c70658b030200be9
Yuval Elmoalem <yuval.elmo@dwalletlabs.com>
2024-10-07 13:21:49 +0300

    removed `data_dir` field in config from serde because it made the wasm function serialize the state differently. removed unused function `should_apply_finality_update_first`

--- consensus/src/consensus.rs
+++ consensus/src/consensus.rs
@@ -937,7 +937,7 @@ impl<R: ConsensusRpc> ConsensusStateManager<R> {
     /// # Note
     /// * This is a decoupled version of the [`self.advance`] function, which separates the
     ///   verification from the fetching of updates.
-    pub fn advance_state(&mut self, updates: AggregateUpdates) -> Result<(), eyre::Error> {
+    pub fn advance_state(&mut self, updates: &AggregateUpdates) -> Result<(), eyre::Error> {
         self.verify_finality_update(&updates.finality_update)?;
         self.apply_finality_update(&updates.finality_update);
 
@@ -945,7 +945,7 @@ impl<R: ConsensusRpc> ConsensusStateManager<R> {
         self.apply_optimistic_update(&updates.optimistic_update);
 
         if self.store.next_sync_committee.is_none() {
-            let updates = updates.updates;
+            let updates = &updates.updates;
 
             if updates.len() == 1 {
                 let update = updates.first().unwrap();
@@ -959,36 +959,6 @@ impl<R: ConsensusRpc> ConsensusStateManager<R> {
         }
 
         Ok(())
-    }
-
-    /// Determines if the provided updates are relevant to the current state.
-    /// Checks if the updates contain a next sync committee update when the current state
-    /// does not have one, or if the attested header slot in the updates is greater than the
-    /// finalized header slot in the current state.
-    /// # Arguments
-    /// * `updates` — A reference to an `AggregateUpdates` struct containing the updates to be
-    ///   checked.
-    /// # Returns
-    /// * Returns `Ok(true)` if we should apply the finality and optimistic updates first,
-    ///   `Ok(false)` otherwise. Returns an error if no updates are found.
-    pub fn should_apply_finality_update_first(&self, updates: &AggregateUpdates) -> Result<bool> {
-        let store_period = calc_sync_period(self.store.finalized_header.slot.into());
-        let update = updates.updates.first().ok_or(eyre!("no updates found"))?;
-        let update = GenericUpdate::from(update);
-        let update_attested_period = calc_sync_period(update.attested_header.slot.into());
-        let update_has_next_committee = self.store.next_sync_committee.is_none()
-            && update.next_sync_committee.is_some()
-            && update_attested_period == store_period;
-
-        if update.attested_header.slot <= self.store.finalized_header.slot
-            && !update_has_next_committee
-        {
-            // Update is irrelevant, we should apply the finality update first.
-            return Ok(true);
-        }
-
-        // Update is relevant, we can apply it first.
-        Ok(false)
     }
 }
 
